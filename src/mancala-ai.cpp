326154ca11392992c7356145c3a48e2365d2a421
Derek Felson <drfelson@gmail.com>
2016-03-08 14:10:48 -0500

    Takes number of stones and holes as args

--- src/mancala-ai.cpp
+++ src/mancala-ai.cpp
@@ -1,11 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+using namespace std;
 
 void usage()
 {
-
+	cerr << "Usage: mancala [stones] [holes]" << endl;
+	cerr << "   where stones in range [2, 6] "
+			"and holes in range [stones-1, 2*(stones-1)]"
+		 << endl;
 }
 
 int main(int argc, char** argv)
 {
+	// Check parameters
+
+	if (argc != 3)
+	{
+		usage();
+		return 1;
+	}
+
+	auto stones = -1;
+	stringstream{argv[1]} >> stones;
+	if (stones < 2 || stones > 6)
+	{
+		usage();
+		return 1;
+	}
+
+	auto holes = -1;
+	stringstream{argv[2]} >> holes;
+	if (holes < stones - 1 || holes > 2* (stones - 1))
+	{
+		usage();
+		return 1;
+	}
+
+	cout << "Holes: " << holes << endl;
+	cout << "Stones: " << stones << endl;
 
 	return 0;
 }
